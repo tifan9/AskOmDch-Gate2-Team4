4acb3bb4f11dcb7eb85affc720e08d050e0c4c1f
Abdoulhakim89 <abdoulbeast89@gmail.com>
2026-02-03 14:59:44 +0200

    Add comment explaining EndPoints class

--- src/test/java/constants/EndPoints.java
+++ src/test/java/constants/EndPoints.java
@@ -1,6 +1,7 @@
 package constants;
 
 public class EndPoints {
+// Each endpoint represents a page from the website
     public static final String STORE = "store";
     public static final String ACCOUNT = "account";
     public static final String CART = "cart";
